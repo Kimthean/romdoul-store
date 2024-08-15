0f7fd6388fa2dc5ff3cab9c2b234944e31a06eb3
Thean <theantian089@gmail.com>
2024-08-15 19:11:03 +0700

    remove redux

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "../node_modules/font-awesome/css/font-awesome.min.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import "./global.css";
 
 import {
@@ -23,20 +21,18 @@ import {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Products />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/product/*" element={<PageNotFound />} />
-      </Routes>
-    </Provider>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product" element={<Products />} />
+      <Route path="/product/:id" element={<Product />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="*" element={<PageNotFound />} />
+      <Route path="/product/*" element={<PageNotFound />} />
+    </Routes>
   </BrowserRouter>
 );
